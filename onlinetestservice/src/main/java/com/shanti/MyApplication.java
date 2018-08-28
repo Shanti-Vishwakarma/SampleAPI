5f3a15a6ab198f521ef8cdadc4fce931be6e9265
Shanti Muchhala <shanti0504@gmail.com>
2018-08-28 23:37:39 +0530

    Initial commit

---
+++ onlinetestservice/src/main/java/com/shanti/MyApplication.java
@@ -0,0 +1,12 @@
+package com.shanti;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("api")
+public class MyApplication extends ResourceConfig {
+    public MyApplication() {
+        packages("com.shanti.service");
+    }
+}
