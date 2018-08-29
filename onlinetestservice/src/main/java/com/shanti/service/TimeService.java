b99052613089f555b159e3e109a8a12391e4b6bb
Shanti Vishwakarma <shanti@zedo.com>
2018-08-29 16:30:30 +0530

    Integrated response object and manager class with service

--- onlinetestservice/src/main/java/com/shanti/service/TimeService.java
+++ onlinetestservice/src/main/java/com/shanti/service/TimeService.java
@@ -1,18 +1,25 @@
 package com.shanti.service;
 
+import com.shanti.manager.TimeProcessManager;
+import com.shanti.response.Time;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.time.LocalDateTime;
 
 @Path("/time")
 public class TimeService {
+    private static Integer counter = 0;
+    private TimeProcessManager processManager = new TimeProcessManager();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTime() {
-        return Response.ok(LocalDateTime.now().toString()).build();
+        counter++;
+        processManager.setInvocationCount(counter);
+        Time time = processManager.execute();
+        return Response.ok(time).build();
     }
 }
